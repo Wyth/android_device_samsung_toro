00cc3c97464e86a3d2bce475dd896d96fc0d3a80
DroidTh3ory <killdroidhack@gmail.com>
2012-12-23 11:52:44 -0500

    Final Fixes.. Maybe
    Change-Id: Id129827f0a7839777e52917514968c7357fa7dc8

--- full_toro.mk
+++ full_toro.mk
@@ -26,10 +26,7 @@ PRODUCT_COPY_FILES += \
     device/samsung/toro/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
     device/sample/etc/apns-conf_verizon.xml:system/etc/apns-conf.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
-    
-# Camera
-PRODUCT_PACKAGES := \
-    Gallery
+
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
