439c2b5edbf9e94c68bdead2a2efa44c8ad70443
droidth3ory <killdroidhack@gmail.com>
2013-01-04 16:05:29 -0500

    Get The FUCK out

--- full_toro.mk
+++ full_toro.mk
@@ -21,9 +21,6 @@
 # lines, full and toro, hence its name.
 #
 
-# Camera and Gallery
-PRODUCT_PACKAGES := \
-    Gallery
 
 #if we do this after the full_base_telephony is included some of these don't get picked up..
 PRODUCT_COPY_FILES += \
